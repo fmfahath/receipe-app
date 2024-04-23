410c6d5decdf71a9c084459655700704205f4657
fmfahath <fmfahath.dev@gmail.com>
2024-04-24 00:54:19 +0530

    Welcome Screen : background img added

--- screens/welcome/Welcome.jsx
+++ screens/welcome/Welcome.jsx
@@ -1,10 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import WelcomeImg from '../../assets/welcome.png';
 
 
 export function Welcome() {
     return (
-        <View>
-            <Text>Welcome Screen</Text>
-        </View>
+        <ImageBackground source={WelcomeImg} style={{ flex: 1 }}>
+            <SafeAreaView>
+                <Text>Welcome Screen</Text>
+            </SafeAreaView>
+        </ImageBackground>
     )
 }
