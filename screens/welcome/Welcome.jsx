5bceb6bbf5ef731b42abab74db6b317847c546e6
fmfahath <fmfahath.dev@gmail.com>
2024-04-24 01:01:35 +0530

    Welcome Screen : logo added

--- screens/welcome/Welcome.jsx
+++ screens/welcome/Welcome.jsx
@@ -1,13 +1,20 @@
-import { View, Text, ImageBackground } from "react-native";
+import { View, Text, ImageBackground, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import WelcomeImg from '../../assets/welcome.png';
-
+import Logo from '../../assets/logo.png';
 
 export function Welcome() {
     return (
         <ImageBackground source={WelcomeImg} style={{ flex: 1 }}>
-            <SafeAreaView>
-                <Text>Welcome Screen</Text>
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}>
+                    <Image source={Logo}></Image>
+                    <Text style={{ color: "white" }}>Welcome Screen</Text>
+                </View>
             </SafeAreaView>
         </ImageBackground>
     )
