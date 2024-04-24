9d42e3f6a8898b7d70bac307adf092a92ea18d62
fmfahath <fmfahath.dev@gmail.com>
2024-04-25 00:39:13 +0530

    RecipeDetails screen : back button added++

--- screens/recipeDetails/RecipeDetails.jsx
+++ screens/recipeDetails/RecipeDetails.jsx
@@ -1,20 +1,29 @@
 import { View, Text, Image } from "react-native"
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { MaterialIcons } from '@expo/vector-icons';
 
-export function RecipeDetails({ route }) {
+export function RecipeDetails({ route, navigation }) {
 
     const { name, image, description } = route.params;
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: "#129575" }}>
             <View>
-                <Text>Back-</Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        // navigation.navigate("Dashboard");
+                        navigation.goBack();
+                    }}
+                >
+                    <MaterialIcons name="arrow-back-ios-new" size={24} color="white" style={{ marginTop: 20, marginLeft: 5 }} />
+                </TouchableOpacity>
             </View>
             <View style={{
                 flex: 1,
                 backgroundColor: "white",
                 marginTop: 150,
-                borderTopLeftRadius: 70,
-                borderTopRightRadius: 70,
+                borderTopLeftRadius: 50,
+                borderTopRightRadius: 50,
                 paddingHorizontal: 20,
 
             }}>
@@ -22,7 +31,7 @@ export function RecipeDetails({ route }) {
                     <Image source={image} style={{
                         height: 170,
                         width: 170,
-                        marginTop: -50,
+                        marginTop: -100,
                         marginBottom: 20
                     }} />
                     <Text style={{
